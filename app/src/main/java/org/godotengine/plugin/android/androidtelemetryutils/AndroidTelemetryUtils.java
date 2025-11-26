bf31ed64b74567ab45333e4a73332ab5d25d9817
Faisa-l <146775603+Faisa-l@users.noreply.github.com>
2025-11-26 13:46:52 +0000

    Add signals to plugin

--- app/src/main/java/org/godotengine/plugin/android/androidtelemetryutils/AndroidTelemetryUtils.java
+++ app/src/main/java/org/godotengine/plugin/android/androidtelemetryutils/AndroidTelemetryUtils.java
@@ -6,7 +6,11 @@ import androidx.annotation.NonNull;
 
 import org.godotengine.godot.Godot;
 import org.godotengine.godot.plugin.GodotPlugin;
+import org.godotengine.godot.plugin.SignalInfo;
 import org.godotengine.godot.plugin.UsedByGodot;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class AndroidTelemetryUtils extends GodotPlugin {
     /**
@@ -25,14 +29,40 @@ public class AndroidTelemetryUtils extends GodotPlugin {
         return BuildConfig.GODOT_PLUGIN_NAME;
     }
 
+    @NonNull
+    @Override
+    public Set<SignalInfo> getPluginSignals()
+    {
+        Set<SignalInfo> signals = new HashSet<>();
+
+        /* To add signal:
+            - Create new SignalInfo class
+            - First argument is the name of the signal
+            - Next arguments are the return types for the signal
+            - Effectively the same as 'signal [NAME](T, T...)' in gdscript
+
+           Connect this signal to a method in Godot. This plugin will invoke the signal.
+         */
+
+        signals.add(new SignalInfo("TestSignal", String.class));
+
+        return signals;
+    }
+
     @UsedByGodot
-    public void DisplayToast()
+    public void DisplayToast(String message)
     {
         getGodot().getActivity().runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                Toast.makeText(getGodot().getActivity(), "Hello World", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getGodot().getActivity(), message, Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @UsedByGodot
+    public void InvokeTestSignal()
+    {
+        emitSignal("TestSignal", "Hello World!");
     }
 }
